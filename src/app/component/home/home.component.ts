0a1b2a32b8de367e440b428bef6250045585def9
Adriana <adrianafh95@gmail.com>
2024-05-03 21:39:59 -0500

    first commit

---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+    // constructor() { };
+
+  // async ngOnInit() {
+
+  //   this.products = await this.storeService.getPromise();
+  //   console.log(this.products)
+
+
+  // };
+
+}
